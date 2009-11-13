a10cb23d94b4e8f7880fe59baf6a9949aa44a441
Manuel Maly <manuel.maly@gmail.com>
2009-11-13 13:30:35 +0100

    added Mainwindowcontroller file

---
+++ src/MainWindowController.h
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.h
+//  WellDone
+//
+//  Created by Manuel Maly on 13.11.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MainWindowController : NSWindowController {
+
+}
+
+@end
---
+++ src/MainWindowController.m
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.m
+//  WellDone
+//
+//  Created by Manuel Maly on 13.11.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "MainWindowController.h"
+
+
+@implementation MainWindowController
+
+- (void) awakeFromNib {
+
+	// Insert Window init code here.		
+}
+
+@end
