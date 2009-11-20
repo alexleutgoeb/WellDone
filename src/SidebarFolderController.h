8ab31a4dc068fd69be53e431744a4ba5ec8df6b1
hatti <hatti@gmx.at>
2009-11-20 16:56:06 +0100

    folder createn

---
+++ src/SidebarFolderController.h
@@ -0,0 +1,16 @@
+//
+//  FolderController.h
+//  WellDone
+//
+//  Created by Christian Hattinger on 20.11.09.
+//  Copyright 2009 TU Wien. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SidebarFolderController : NSViewController {
+IBOutlet NSObjectController* folderArrayController;
+}
+
+@end
---
+++ src/SidebarFolderController.m
@@ -0,0 +1,23 @@
+//
+//  FolderController.m
+//  WellDone
+//
+//  Created by Christian Hattinger on 20.11.09.
+//  Copyright 2009 TU Wien. All rights reserved.
+//
+
+#import "SidebarFolderController.h"
+
+
+@implementation SidebarFolderController
+
+- (id) init
+{
+	self = [super initWithNibName:@"FolderView" bundle:nil];
+	if (self != nil)
+	{		
+	}
+	return self;
+}
+
+@end
