58a7dbe25fe6a56e52a49bcf3ba2bc08b60a7274
alexleutgoeb <privat@alexleutgoeb.com>
2009-11-28 19:28:46 +0100

    Added empty SyncManager class.

---
+++ src/SyncManager.h
@@ -0,0 +1,16 @@
+//
+//  SyncManager.h
+//  WellDone
+//
+//  Created by Alex Leutgöb on 28.11.09.
+//  Copyright 2009 alexleutgoeb.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SyncManager : NSObject {
+
+}
+
+@end
---
+++ src/SyncManager.m
@@ -0,0 +1,14 @@
+//
+//  SyncManager.m
+//  WellDone
+//
+//  Created by Alex Leutgöb on 28.11.09.
+//  Copyright 2009 alexleutgoeb.com. All rights reserved.
+//
+
+#import "SyncManager.h"
+
+
+@implementation SyncManager
+
+@end
