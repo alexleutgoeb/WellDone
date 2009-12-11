de9a94bdc944bd721a5d8c524ff189d70684a5c1
alexleutgoeb <privat@alexleutgoeb.com>
2009-12-11 22:30:49 +0100

    Added dummy files for preferences window.

---
+++ src/SyncPreferences.h
@@ -0,0 +1,30 @@
+//
+//  SyncPreferences.h
+//  WellDone
+//
+//  Created by Alex Leutgöb on 11.12.09.
+//  Copyright 2009 alexleutgoeb.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "GtdApi.h"
+#import "SS_PreferencePaneProtocol.h"
+
+
+@interface SyncPreferences : NSViewController <SS_PreferencePaneProtocol> {
+@private
+    IBOutlet NSScrollView *scrollView_accountList;
+    IBOutlet NSTableView *tableView_accountList;
+	IBOutlet NSButton *button_editService;
+	IBOutlet NSTextField *textField_overview;
+	
+	NSArray *serviceArray;
+}
+
+// - (void)editService:(id<GtdApi> *)aService;
+// - (IBAction)editSelectedService:(id)sender;
+// - (void)updateServiceOverview;
+// - (void)updateControlAvailability;
+// - (NSString *)statusMessageForService:(id<GtdApi> *)aService;
+
+@end
---
+++ src/SyncPreferences.m
@@ -0,0 +1,58 @@
+//
+//  SyncPreferences.m
+//  WellDone
+//
+//  Created by Alex Leutgöb on 11.12.09.
+//  Copyright 2009 alexleutgoeb.com. All rights reserved.
+//
+
+#import "SyncPreferences.h"
+
+
+@implementation SyncPreferences
+
+- (id)init {
+	if (self = [super initWithNibName:@"SyncPreferences" bundle:nil]) {
+		
+	}
+	return self;
+}
+
+
+#pragma mark SS_PreferencePaneProtocol methods
+
++ (NSArray *)preferencePanes {
+    return [NSArray arrayWithObjects:[[[SyncPreferences alloc] init] autorelease], nil];
+}
+
+
+- (NSView *)paneView {
+    return self.view;
+}
+
+
+- (NSString *)paneName {
+    return @"Sync";
+}
+
+
+- (NSImage *)paneIcon {
+    return nil;
+}
+
+
+- (NSString *)paneToolTip {
+    return @"Sync Services";
+}
+
+
+- (BOOL)allowsHorizontalResizing {
+    return NO;
+}
+
+
+- (BOOL)allowsVerticalResizing {
+    return NO;
+}
+
+@end
