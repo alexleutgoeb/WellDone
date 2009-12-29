2322fc04da327709de2e642efa9ea9115f40f5e6
alexleutgoeb <privat@alexleutgoeb.com>
2009-12-29 21:20:17 +0100

    Add doc comments to SyncController.h.

--- src/SyncController.h
+++ src/SyncController.h
@@ -11,17 +11,40 @@
 @class SyncController, SyncManager;
 
 
-// Protocol for the parser to communicate with its delegate.
+/**
+ SyncControllerDelegate protocol provides callback methods for communicating with 
+ a delegate while syncing.
+*/
 @protocol SyncControllerDelegate <NSObject>
 
 @optional
+
+/**
+ Callback method for notifying the delegate about a successful sync.
+ @param sc the sync controller instance
+*/
 - (void)syncControllerDidSyncWithSuccess:(SyncController *)sc;
+
+/**
+ Callback method for notifying the delegate about conflicts in sync.
+ @param sc the sync controller instance
+*/
 - (void)syncControllerDidSyncWithConflicts:(SyncController *)sc;
+
+/**
+ Callback method for notifying the delegate about an error while syncing.
+ @param sc the sync controller instance
+ @param error the emerged error during syncing
+*/
 - (void)syncController:(SyncController *)sc didSyncWithError:(NSError *)error;
 
 @end
 
 
+/**
+ SyncController class
+ The class is the main class for enabling services and trigger a sync.
+*/
 @interface SyncController : NSObject {
 @private
 	// List of available sync services
@@ -33,11 +56,29 @@
 	NSOperationQueue *syncQueue;
 }
 
+/**
+ Getter for a list of all available syncServices.
+*/
 @property(nonatomic, readonly) NSMutableDictionary *syncServices;
 
+/**
+ Enables a syncService in the sync controller.
+ @param anIdentifier specific syncService identifer to enable
+ @param aUser username for login
+ @param aPwd password for login
+*/
 - (BOOL)enableSyncService:(NSString *)anIdentifier withUser:(NSString *)aUser andPwd:(NSString *)aPwd;
+
+/**
+ Disables a specific syncService in the sync controller.
+ @param anIdentifier the specific syncService identifiert to disable
+*/
 - (BOOL)disableSyncService:(NSString *)anIdentifier;
 
+/**
+ Starts the sync in background. Notifications about success and failures will be 
+ sent through the sync controller delegate.
+*/
 - (void)sync;
 
 @end
